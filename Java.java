33f3eeae0fc7887143bb81b6b3e6a082de8518c9
clevernovaesneto24 <60674364+clevernovaesneto24@users.noreply.github.com>
2020-08-06 20:09:48 -0300

    Update Java.java

--- Java.java
+++ Java.java
@@ -1,7 +1,6 @@
-"Hello.java"
 public class Hello { 
-	public static void main(String[] args {
-		System.ou.println("Hello, Arbyte!");
+	public static void main(String[] args) {
+		System.out.println("Olá, Arbyte!");
 	}
 }
 
